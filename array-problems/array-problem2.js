4414cc21a711df74ea800a3a305fb15ca916b7a3
Martin Reece <martin@Martins-Air.home>
2022-03-03 16:24:18 -0500

    I did a minor change to my solution to this array-problem2

--- array-problems/array-problem2.js
+++ array-problems/array-problem2.js
@@ -1,9 +1,11 @@
-//Step 1: Create a function name joinTogether that takes in an array of elements. I named this array (Colors)
-//Step 2: Create an array name (arrayOfColors) and assigned this array with different 
+//Step 1: Create a function name joinTogether that takes in an array of elements. I named this array (arrayOfColors)
+//Step 2: Create a variable called commaArray and assigned my parameter(arrayOfColors) with the join() method. This join() method will create and return a new string by concatenating all the elements in an array with a comma.
+//Step 3: Print to the console my variable (commaArray)
+//Step 4: Execute my joinTogether function as it pass in my arrayofcolors parameter.
 
 
-function joinTogether(colors){
-    let commaArray = colors.join(',');
+function joinTogether(arrayOfColors){
+    let commaArray = arrayOfColors.join(',');
     console.log(commaArray);
 }
 
