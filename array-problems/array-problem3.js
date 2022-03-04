540ec5b85a8fa2e6d83752d29f2e31f9a8cc7222
Martin Reece <martin@Martins-Air.home>
2022-03-04 12:44:40 -0500

    I updated my solution to array-problem3 and added the reverse method on my newArray variable so it can sort those numbers from greatest to least

--- array-problems/array-problem3.js
+++ array-problems/array-problem3.js
@@ -1,11 +1,11 @@
 //Step 1: Create a function name (sortedArray) that takes in an array which I named as (myArray)
-//Step 2: Create a variable name (newArray) and assigned it to my parameter(myArray) and used the sort() method to sort out the elements of (myArray) and return the sorted array by printing to the console.
-//Step 3: Execute the function sortedArray 
+//Step 2: Create a variable name (newArray) and assigned it to my parameter(myArray) and used the sort() method to sort out the elements of (myArray) and return the sorted array
+//Step 3: Execute the function sortedArray to print to the console
 
 
 function sortedArray(myArray){
-    let newArray = myArray.sort();
-    console.log(newArray);
+    let newArray = myArray.sort().reverse();
+    return newArray;
 }
 
-sortedArray([-3,8,7,6,5,-4,3,2,1]);
+console.log(sortedArray([-3,8,7,6,5,-4,3,2,1]));
