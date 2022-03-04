a8d6d5b3ff134ae6d00a4de90f4718770e482b3d
Martin Reece <martin@Martins-Air.home>
2022-03-04 12:26:45 -0500

    I changed my answer to the solution of array-problem1

--- array-problems/array-problem1.js
+++ array-problems/array-problem1.js
@@ -1,11 +1,11 @@
 //Step 1: Create a function name firstTwo that takes two parameters an array(fruits) and a number (n)
 //Step 2: Create a variable name slicedArray and assigned my array(fruits) with the slice method to return selected elements in an array as a new array.  This slice() method selects from a given start which is 0 up to a given end which is 2.
-//Step 3: Print to the console the variable "sliceArray"
-//Step 4: Execute the function firstTwo (['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'], 2)
+//Step 3: Returned the variable "sliceArray"
+//Step 4: Execute the function firstTwo (['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'], 2) to print to the console.
 
 function firstTwo(fruits, n){
-    let slicedArray = fruits.slice(0, 2);
-    console.log(slicedArray);
+    let slicedArray = fruits.slice(0, n);
+    return slicedArray;
 }
 
-firstTwo(['Banana','Orange','Lemon','Apple','Mango'], 2);
+console.log(firstTwo(['Banana','Orange','Lemon','Apple','Mango'], 2));
