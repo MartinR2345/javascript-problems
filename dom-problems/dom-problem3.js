ccd966ce98ba3ca022e9f7e4cf11a1c474fa7324
Martin Reece <martin@Martins-Air.home>
2022-03-07 13:23:07 -0500

    This is two new files for dom-problems. One HTML and one is for JS

---
+++ dom-problems/dom-problem3.js
@@ -0,0 +1,8 @@
+//Step 1: Create an HTML page with a button and a dropdown list with three items
+//Step 2: Write a javascript function that displays the number of items and displays the item in the list when you select from the dropdown
+
+
+
+function myFunction() {
+    var x = document.getElementById("mySelect").value;
+    document.getElementById("demo").innerHTML = "You selected: " 
