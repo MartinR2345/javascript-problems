80b90092fda5fc6128c618ba69a4b02814d1894d
Martin Reece <martin@Martins-Air.home>
2022-03-07 14:01:08 -0500

    I wrote down a brief objective for dom-problem3 JS file

--- dom-problems/dom-problem3.js
+++ dom-problems/dom-problem3.js
@@ -1,8 +1,9 @@
-//Step 1: Create an HTML page with a button and a dropdown list with three items
-//Step 2: Write a javascript function that displays the number of items and displays the item in the list when you select from the dropdown
 
+//JS OBJECTIVE
+//Step 1: Write a javascript function that displays the number of items and displays the item in the list when you select from the dropdown
 
 
 function myFunction() {
-    var x = document.getElementById("mySelect").value;
-    document.getElementById("demo").innerHTML = "You selected: " 
+
+    
+}
