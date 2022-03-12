5dace6c4c06f136e7b5e4220f92f34efb4369522
Martin Reece <martin@Martins-Air.home>
2022-03-12 07:42:10 -0500

    I made a mistake by declaring the wrong function name.  Now it is corrected to function removeProperty

--- objects-problems/objects-problem1.js
+++ objects-problems/objects-problem1.js
@@ -1,7 +1,7 @@
 //Step 1: Create an sample object name person and assigned it three key-value pairs outside of my function
 //Step 2: Create a function name removeProperty that takes in two parameters (object, property) and delete a key/property. I choose a property to delete.
 //Step 3: Use the delete operator to delete a property from an object as I access my object by using the square brackets property accessor
-//Step 4: Return my object parameter
+//Step 4: Return my object parameter (object)
 //Step 5: Print to the console my function removeProperty of the object and the property I want to delete
 
 const person = {
