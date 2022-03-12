241e7d2aec9a6f2f0e57ab51bdd3f18c01f36592
Martin Reece <martin@Martins-Air.home>
2022-03-12 17:21:33 -0500

    I wrote out the steps to objects-problem3 file

---
+++ objects-problems/objects-problem3.js
@@ -0,0 +1,5 @@
+//Step 1: Declare an object name items and this object has three(3) items
+//Step 2: Create a function name calculate that accepts an object as a parameter and tells you the length (tells you how many items are in it) of the object
+//Step 3: I'm going to return and use the Object.keys() method along with the length property to get the length of this object.  This will return an array of a given object's list property names and the length property returns the number of elements in that array.  
+//Step 4: Print to console the function calculate 
+
