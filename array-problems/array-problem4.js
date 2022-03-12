9148c542325c7a299e83ede4b5adfdffb00401aa
Martin Reece <martin@Martins-Air.home>
2022-03-11 20:13:17 -0500

    I added the steps to array-problem4

---
+++ array-problems/array-problem4.js
@@ -0,0 +1,7 @@
+//Step 1: Create a function name removeDuplicates that takes one parameter, an array (arr)
+//Step 2: Create an empty array use as a default reference
+//Step 3: Use a for loop to loop through each and every element of my array
+//Step 4: Create an if statement that will check if the specialArray includes or has that element or not. I would be using the includes() method. If not, then if the condition becomes true, then we push that element into the special array with the push() method
+//Step 5: Return my specialArray variable
+//Step 6: Print to the console the function removeDuplicates 
+
