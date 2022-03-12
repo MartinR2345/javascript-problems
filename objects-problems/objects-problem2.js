399fc821b794b1cc7dccda78b1e0dfcdae48b229
Martin Reece <martin@Martins-Air.home>
2022-03-12 08:54:26 -0500

    I wrote out my steps to answer objects-problems2

---
+++ objects-problems/objects-problem2.js
@@ -0,0 +1,5 @@
+//Step 1: Create an array of people objects with firstName, LastName and Age
+//Step 2: Create a function name displayFirstAndLastNames that takes in an array of objects which I named as (arrOfObjects)
+//Step 3: Then I take the parameter (arrOfArray) and use the for each() method with it.  This will loop over the elements of my array. So whatever function I pass in, that function will be called once per item, where each item will be passed into the function automatically.
+//Step 4: Now inside the parenthesis() in my foreach property, this expects me to pass in a callback.  I will use an anonymous function expression that will take in one parameter I called (give)
+//Step 5: Print to console the parameter (give) first and last names of all the people of my array of objects 
