acad5415d4a02b41e172ba2e0fef5c8cfddf1a14
Martin Reece <martin@Martins-Air.home>
2022-03-12 07:34:54 -0500

    I wrote my answer to the objects-problem1

--- objects-problems/objects-problem1.js
+++ objects-problems/objects-problem1.js
@@ -3,3 +3,18 @@
 //Step 3: Use the delete operator to delete a property from an object as I access my object by using the square brackets property accessor
 //Step 4: Return my object parameter
 //Step 5: Print to the console my function removeProperty of the object and the property I want to delete
+
+const person = {
+    firstName: "Mary",
+    lastName: "Bailey",
+    age: 35
+  };
+  
+  
+  function removeProperty (object, property) {
+    delete object[property];
+    return object;
+    
+  }
+  
+  console.log(deleteOne(person, 'firstName'));
