54b06da4eb4d66f79b1e37f1f56de5ed3cf1f9bd
Martin Reece <martin@Martins-Air.home>
2022-03-13 09:08:16 -0400

    I wrote out my steps to objects-problem4 file

---
+++ objects-problems/objects-problem4.js
@@ -0,0 +1,5 @@
+//Step 1: Create an object name bottle that as a couple of key-pair values
+//Step 2: Write a function name checkItem and it has two parameters (obj) and (item) this function will check to see whether an object contains the specified key
+//Step 3: I am going to return my first parameter (obj) that been assigned to hasOwnProperty method which holds my second parameter (item).  Basically this will return a boolean indicating whether the object has the specified property as its own property
+//Step 4: Print to the console my function checkItem
+
