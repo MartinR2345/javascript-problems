c7bf497b3aa7aa3d378f3eb9259947ef667bcb80
Martin Reece <martin@Martins-Air.home>
2022-03-14 09:35:45 -0400

    I wrote down the steps to this recursion-problem 1

---
+++ recursion-problems/recursion-problem1.js
@@ -0,0 +1,6 @@
+//Objective: Write a JavaScript function to calculate the factorial of a number.
+
+//Step 1: Write a function name factorial that takes in one parameter name (num) and the goal is to calculate the factorial of a number.
+//Step 2: I'm going to use an if statement to prevent infinite recursion and to check if my parameter (num) is equal to 0 then return 1.  If the (num) is positive or greater then the program will loop itself by decreasing the number until that number becomes 1.  When the number reaches 0, 1 is returned.
+
+
