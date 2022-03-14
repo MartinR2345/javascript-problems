4fb7909113b177ddb58c16743a8aaa79f50c6336
Martin Reece <martin@Martins-Air.home>
2022-03-14 13:05:55 -0400

    I made a couple of changes to the steps I wrote previously for this recursion-problem 2 file

--- recursion-problems/recursion-problem2.js
+++ recursion-problems/recursion-problem2.js
@@ -1,8 +1,8 @@
 //Objective: Write a JavaScript function to compute the sum of an array of integers.
 
 //Step 1: Write a function name sumArray that takes in one parameter called (numbers).  This is actually holding my array value.
-//Step 2: I create a variable name store and set it equal to zero(0).  This will store the sum of my array.
-//Step 3: I used a for loop to loop through my numbers array individually  
+//Step 2: I initialize a variable name sum as 0 to store the result 
+//Step 3: I used a for loop to loop through my numbers array individually and add them to the sum of the array.  
 //Step 4: Return my (sum) variable once the loop is finished and print to the console my function sumArray
 
 function sumArray (numbers){
