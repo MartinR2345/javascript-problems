356a92e8d0d160bee2c2055b2c1307013467fb87
Martin Reece <martin@Martins-Air.home>
2022-03-14 12:57:55 -0400

    I wrote out my solution to recursion-problem 2

--- recursion-problems/recursion-problem2.js
+++ recursion-problems/recursion-problem2.js
@@ -5,3 +5,25 @@
 //Step 3: I used a for loop to loop through my numbers array individually  
 //Step 4: Return my (sum) variable once the loop is finished and print to the console my function sumArray
 
+function sumArray (numbers){
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++){
+      sum += numbers[i]
+    }
+  return sum;
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]))
+
+
+//shorter way would be:
+
+//Step 1: Write a function name sumArray that takes in one parameter called (numbers).  This is actually holding my array value.
+//Step 2: Create a variable name sum and assigned it to my parameter (numbers)
+//Step 3: Use or attach the reduce() method on my (numbers) array. This will take my array and reduce it down to a single value.
+//Step 4: Return my variable sum and print to the console my function sumArray
+
+function sumArray (numbers){
+    const sum = numbers.reduce((a, b) => a + b)
+    return sum
+}
+console.log(sumArray([1, 2, 3, 4, 5, 6]))
