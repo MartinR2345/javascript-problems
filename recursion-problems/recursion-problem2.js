d54648c955bdbcf58998a5e5ff0e531b8eaccac4
Martin Reece <martin@Martins-Air.home>
2022-03-15 12:25:44 -0400

    I made sure to answer this problem using recursion.

--- recursion-problems/recursion-problem2.js
+++ recursion-problems/recursion-problem2.js
@@ -1,5 +1,22 @@
 //Objective: Write a JavaScript function to compute the sum of an array of integers.
 
+//Step 1: Create a function name sumOfArray that takes one parameter which would be our array(arr) of integers
+//Step 2: I use a if statement to create a condition(base condition) and used as well to prevent infinite recursion.
+//Step 3: The else statement is use to check our condition(base condition) over and over until our condition is met or until our condition becomes true. Once that happens the function stops calling itself.
+//Step 4: Print to the console the function sumOfArray
+
+
+function sumOfArray(arr){
+  if (arr.length === 0){
+      return 0;
+  } else{
+     return arr.shift() + sumOfArray(arr);
+  }
+}
+
+console.log(sumOfArray([1,2,3,4,5,6]))
+
+
 //Step 1: Write a function name sumArray that takes in one parameter called (numbers).  This is actually holding my array value.
 //Step 2: I initialize a variable name sum as 0 to store the result 
 //Step 3: I used a for loop to loop through my numbers array individually and add them to the sum of the array.  
