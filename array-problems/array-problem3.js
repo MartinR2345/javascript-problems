77acc6f0ffa108e1b1db66c4b27b2063441a2f13
Martin Reece <martin@Martins-Air.home>
2022-04-20 09:48:32 -0400

    Error corrected

--- array-problems/array-problem3.js
+++ array-problems/array-problem3.js
@@ -1,7 +1,7 @@
-//Step 1: Create a function name (sortedNumbers) that takes in an array of unsorted numbers (arr)
-//Step 2: Inside this function, I returned the arr with the sort() method and the reverse() method
-//Step 3: Why? Because the original array(arr) are out of place, the sort() method sorts and changes the positions of the elements in the original array putting those elements in place. 
-//Step 4: Then print to the console my function sortedNumbers with the value of arr 
+//Step 1: I created a function called (sortNumbers) that takes in an array (arr)
+//Step 2: And inside this function, I'm returning the arr with the sort() method
+//Step 3: Why? Because of the elements inside of the original array(arr) are out of place, the sort() method sorts and changes the positions of the elements in the original array putting those elements in place. 
+//Step 4: Then print to the console my function sortNumbers with the value of arr 
 
 
 function sortNumbers(arr){
@@ -13,10 +13,10 @@ console.log(sortNumbers([-3,8,7,6,5,-4,3,2,1]));
 
 
 
-//Step 1: Create a function name (sortedNumbers) that takes in an array of unsorted numbers (arr)
-//Step 2: Inside this function, I returned the arr with the sort() method and the reverse() method
-//Step 3: Why? Because the original array(arr) are out of place, the sort() method sorts and changes the positions of the elements in the original array putting those elements in place. The reverse() method basically makes the first array element becomes the last, and the last array element becomes the first.
-//Step 4: Then print to the console my function sortedNumbers with the value of arr 
+//Step 1: I created a function called (sortNumbers) that takes in an array (arr)
+//Step 2: And inside this function, I'm returning the arr with the sort() method and the reverse() method
+//Step 3: Why? Because of the elements inside of the original array(arr) are out of place, the sort() method sorts and changes the positions of the elements in the original array putting those elements in place. The reverse() method basically makes the first array element becomes the last, and the last array element becomes the first.
+//Step 4: Then print to the console my function sortNumbers with the value of arr 
 
 
 function sortNumbers(arr){
