da96ff41225df3c016321c8a8a13866987da4171
Martin Reece <martin@Martins-Air.home>
2022-04-20 18:03:37 -0400

    I made some changes to the explanation of problem1

--- functions-problems/problem1.js
+++ functions-problems/problem1.js
@@ -1,20 +1,19 @@
 //Goal: What I'm I trying to do? Write a function that find the longest word withing the string.
 
-//Step 1: Create a function name findLongestWord that accepts a string(str) as a parameter
-//Step 2: Create a variable name newArray and set it to string(str). This string(str) will use the split() method to turn this string into an array of each individual characters so I can loop through each individual character and this will also tell me the length of it as well.
-//Step 3: Create a variable name longest and set it to an empty string.  Why? The length of an empty is 0 and I can use this to compare in my for loop
-//Step 4: Create a for loop. Why? This will loop over my array
-//Step 5: Create a if statment. Why? This will check to see if my newArray[i].length is greater than my longest.length varable and if it is then longest will be updated to newArray[i]
-//Step 6: Return longest variable then print to the console findLongestWord("I am grateful") which will give me "grateful" as the longest word
+//Step 1: Create a function called findLongestWord that accepts a string(str) as a parameter
+//Step 2: The first thing I'm doing in this function is create a variable called arrOfWords. Why? I'm turning my string into an array of words with a space in between each word with the split(" ") method
+//Step 3: The second thing I'm doing is create a variable name longestWord. Why? This variable is use to be compare with my arrOfWords in the for loop.  The length of an empty string is 0 and I can use this to compare in my for loop
+//Step 4: The third thing I'm doing is creating a loop. Why? I'm checking to see if the first word of my arrOfWords length is greater than the length of longestWord then update longestWord.
+//Step 6: Return longestWord variable then print to the console findLongestWord("I am grateful") which will give me "grateful" as the longest word
 
 function findLongestWord(str){
-    let newArray = str.split(" ");
-    let longest = "";
-    for(let i = 0; i < newArray.length; i++){
-        if (newArray[i].length > longest.length)
-        longest = newArray[i];
+    let arrOfWords = str.split(" ");
+    let longestWord = "";
+    for(let i = 0; i < arrOfWords.length; i++){
+        if (arrOfWords[i].length > longestWord.length)
+        longestWord = arrOfWords[i];
     }
-    return longest;
+    return longestWord;
 }
 
 console.log(findLongestWord("I am grateful"));
