aef449bc6927234080495febf85b5966add0c924
Martin Reece <martin@Martins-Air.home>
2022-04-26 12:29:19 -0400

    Error corrected

--- functions-problems/problem3.js
+++ functions-problems/problem3.js
@@ -1,9 +1,9 @@
 //Goal: What is the objective? Write a function that will count the number of occurrences of the specified letter within the string.
 
 //Step 1: Create a function name countLetter that accepts two arguments/parameters string(str) and letter(letter)
-//Step 2: Create a variable name letterCount and set it to 0. This will keep track of counting the letters after every iteration from my
+//Step 2: Create a variable name letterCount and set it to 0. This will keep track of counting the letters after every iteration from my loop
 //Step 3: Create a loop that will inspect each letter one by one in the string(str)? Why? This loop is checking to see if letter "e" is in the string at a specified index/position. If it is then update "letterCount" by one.
-//Step 4: Return the letterCount and  print to console my function countLetter("live simply and be grateful", "e") which will give me "3"
+//Step 4: Return the letterCount and print to console my function countLetter("live simply and be grateful", "e") which will give me "3"
 
 
 function countLetter(str, letter){
