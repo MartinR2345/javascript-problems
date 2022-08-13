cb30af4afb62c242a51fdb8115a263220adaf8f7
Martin Reece <martin@Martins-Air.home>
2022-08-13 13:08:14 -0400

    I updated my code and wrote a better explanation of my answer

--- functions-problems/problem4.js
+++ functions-problems/problem4.js
@@ -1,19 +1,22 @@
-//Step 1: Create a function name fizzBuzz that has no parameters, no arguments
-//Step 2: Create a for loop that loops over each number of the integers from 1 to 100
-//Step 3: Create an if else statement.  This first if statement will use a modulo operator(%) and logical AND(&&) to check and see if the remainder is equal to zero when it is divided by 3 & 5. If it's true, then print to console "FizzBuzz"
-//Step 4: Create an else if statement.  This else if statement will check for multiples of 3 and this will print to console "Fizz"
-//Step 5: Create an else if statement.  This else if statement will check for multiples of 5 and this will print to console "Buzz"
-//Step 6: Create an else statement.  This else statement will print to the console variable (i) if none of the conditions are true.
+//Step 1: Create a function name fizzBuzz() that has no parameters, no arguments and the first thing I'm doing inside this function is,
+//Step 2: I create a for loop that loops through the integers from 1 to 100 then 
+//Step 3: I declare a variable called "word" and assigned it to an empty strings(""). The length of an empty string("") is 0 and this reduce conditionals and allows me to check two things instead of three things. 
+//Step 4: Create an if else statement.  This first if statement will use a modulo operator(%) and logical AND(&&) to check and see if the remainder is equal to zero when it is divided by 3. If it's true, then update the variable "word" to "fizz"
+//Step 5: Create an if else statement.  This second if statement will use a modulo operator(%) and logical AND(&&) to check and see if the remainder is equal to zero when it is divided by 5. If it's true, then update the variable "word" to "buzz"
+//Step 6: Log to the console of "i" and "word" and call the function fizzBuzz()
 
-function fizzBuzz() {
-    for (let i = 0; i <= 100; i++) {
-        if (i % 3 === 0 && i % 5) {
-            console.log("FizzBuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz");
-        } else if (i % 5 === 0) {
-            console.log("Buzz");
-        } else console.log(i);
+const fizzBuzz = () => {
+    for(let i = 1; i <= 100; i++){
+      let word = "";
+      
+      if (i % 3 === 0){
+        word += 'fizz'
+      }
+       if (i % 5 === 0){
+        word += 'buzz'
+      }
+      console.log(i, word);
     }
-}
-fizzBuzz();
+  }
+  
+  fizzBuzz();
