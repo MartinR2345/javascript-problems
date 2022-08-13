7cc6df00c99c09b2fefe53733b16a8bc355d9acd
Martin Reece <martin@Martins-Air.home>
2022-08-13 12:50:52 -0400

    I updated my code and wrote a better explanation of my answer

--- functions-problems/problem3.js
+++ functions-problems/problem3.js
@@ -1,19 +1,24 @@
 //Goal: What is the objective? Write a function that will count the number of occurrences of the specified letter within the string.
 
-//Step 1: Create a function name countLetter that accepts two arguments/parameters string(str) and letter(letter)
-//Step 2: Create a variable name letterCount and set it to 0. This will keep track of counting the letters after every iteration from my loop
-//Step 3: Create a loop that will inspect each letter one by one in the string(str)? Why? This loop is checking to see if letter "e" is in the string at a specified index/position. If it is then update "letterCount" by one.
-//Step 4: Return the letterCount and print to console my function countLetter("live simply and be grateful", "e") which will give me "3"
+//Step 1: I created a function called letterCount that takes in two arguments/parameters a string(str) and a letter(letter).  Inside this function
+//Step 2: Declare a variable name currentLetterCount and set it to 0. This will keep track of counting the letters after every iteration from forEach() method
+//Step 3: This forEach() method will inspect each letter one by one to check and see if the letter "a" matches a letter in my letter in array variable then update "currentLetterCount" by one.
+//Step 4: Return the currentLetterCount and log to the console my function letterCount("Apples oranges mangos pear", "a") which will give me "4"
 
 
-function countLetter(str, letter){
-    let letterCount = 0;
-    for (let i = 0; i < str.length; i++) {
-        if (str.charAt(i) === letter){
-            letterCount ++
-        }
-    }
-    return letterCount;
-}
-console.log (countLetter("live simply and be grateful", "e"));
+const letterCount = (str, letter) => {
+    let currentLetterCount = 0;
+    
+    const arrayOfLetters = str.split("");
+    
+    arrayOfLetters.forEach((letterInArray) => {
+      if(letterInArray.toLowerCase() === letter.toLowerCase()) {
+        currentLetterCount += 1;
+      }
+    })
+        return currentLetterCount
+  }
+  
+  
+  console.log(letterCount("Apples oranges mangos pear", "a"));
 
