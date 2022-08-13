d4405853e7eaa3eaa4ebced0a870753eb145bcf2
Martin Reece <martin@Martins-Air.home>
2022-08-13 14:36:27 -0400

    I updated my code and wrote a better explanation of my answer

--- objects-problems/objects-problem1.js
+++ objects-problems/objects-problem1.js
@@ -1,8 +1,8 @@
 //Step 1: Create an sample object name person and assigned it three key-value pairs outside of my function
-//Step 2: Create a function name removeProperty that takes in two parameters (object, property) and delete a key/property. I choose a property to delete.
-//Step 3: Use the delete operator to delete a property from an object as I access my object by using the square brackets property accessor
-//Step 4: Return my object parameter (object)
-//Step 5: Print to the console my function removeProperty of the object and the property I want to delete
+//Step 2: I created a function name removeProperty that takes in two parameters (object, property)
+//Step 3: Inside this function I'm using the delete operator to delete a property which is 'firstName' from an 'person' object
+//Step 4: Then I'm returning this 'person' object to the console
+//Step 5: Log to the console my function removeProperty of the 'person' object and the property 'firstName' I want to delete
 
 const person = {
     firstName: "Mary",
@@ -11,10 +11,10 @@ const person = {
   };
   
   
-  function removeProperty (object, property) {
+  const removeProperty = (object, property) => {
     delete object[property];
     return object;
     
   }
   
-  console.log(removeProperty (person, 'firstName'));
+  console.log(removeProperty(person, 'firstName'));
